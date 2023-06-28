7982a42498cff23364aa23b0bda50906a7b4ee96
José Francisco <franciscojosefrancisco341@gmail.com>
2023-06-28 11:23:41 -0300

    Continuação
    Modificações do zero, depois de entender as funções

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Cabecalho from "./componentes/Cabecalho";
+import Rodape from "./componentes/Rodape";
 /*
 Arquivo App JSX
 Componente funcional = porque ele é uma função
@@ -9,22 +10,8 @@ function App() {
   return (
     /*O React substitui className por class porque class é palavra resrvada do JS */
     <>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         José Francisco
-        </p>
-        <a
-          className="App-link"
-          href="https://google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Google
-        </a>
-      </header>
-    </div>
+      <Cabecalho/>
+      <Rodape/>
     </>
   );
 }
