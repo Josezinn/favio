5d962c25e73428bff2b3707024135dfddef0d84b
José Francisco <franciscojosefrancisco341@gmail.com>
2023-09-29 10:40:53 -0300

    Continuação Aula 29 de Setembro 2023

--- src/componentes/Conteudo/index.js
+++ src/componentes/Conteudo/index.js
@@ -1,51 +1,63 @@
-import React, { useState } from 'react'
-import styles from './Conteudo.module.css'
+import React, { useState } from "react";
+import styles from "./Conteudo.module.css";
 
 const Conteudo = () => {
+  const [nomeSite, setNomeSite] = useState("");
+  const [url, setUrl] = useState("");
+  const [favoritos, setFavoritos] =useState([])
 
-  const [nomeSite,setNomeSite]=useState('')
-  const [url,setUrl]=useState('')
-
-  let favoritos=[]
 
   function salvarFavorito() {
-    console.log('favoritos>>>:', favoritos)
-    console.log('estados>>>:', {nomeSite, url})
-    favoritos=[...favoritos, {nomeSite,url}]
-    console.log('favoritos 2>>>:', favoritos)
+    console.log("favoritos>>>:", favoritos);
+    console.log("estados>>>:", { nomeSite, url });
+    setFavoritos([...favoritos, { nomeSite, url }])
+    console.log("favoritos 2>>>:", favoritos);
     localStorage.setItem("favorito", JSON.stringify(favoritos));
   }
 
   return (
+    <>
+    <ul>
+        {favoritos[0]!==undefined &&
+          favoritos.map((favorito) => (
+            <li>{favorito.nomeSite} : {favorito.url}</li>
+          
+          ))}
+    </ul>
     <div className={styles.area}>
-        Conteudo
-    <form className={styles.formulario} onSubmit={()=>alert("Favorito salvo")}>
-        <label>
-            Nome do Site
-        </label> 
-          <h1>{nomeSite}</h1>
-        <br/>
-        <input name='nome_site' 
-        onChange={(evento) => {
-          setNomeSite(evento.target.value);
-          console.log(nomeSite);
-        }}
+      
+      <h1>Conteudo</h1>
+      <form
+        className={styles.formulario}
+      >
+        <label>Nome do Site</label>
+        <h1>{nomeSite}</h1>
+        <br />
+        <input
+          name="nome_site"
+          onChange={(evento) => {
+            setNomeSite(evento.target.value);
+            console.log(nomeSite);
+          }}
         ></input>
-        <br/>
-        <label>
-            URL
-        </label> 
+        <br />
+        <label>URL</label>
         <h1>{url}</h1>
-        <br/>
-        <input type='url' name='url'
-         onChange={(evento) => {
-          setUrl(evento.target.value);
-          console.log(url);
-        }}        
+        <br />
+        <input
+          type="url"
+          name="url"
+          onChange={(evento) => {
+            setUrl(evento.target.value);
+            console.log(url);
+          }}
         ></input>
-        <button onClick={()=> salvarFavorito}>Salvar</button>
-    </form>
+        <input type="button" value="Salvar"
+        onClick={()=> salvarFavorito()}/>
+        {/*<button onClick={() => salvarFavorito}>Salvar</button>*/}
+      </form>
     </div>
-  )
-}
+    </>
+  );
+};
 export default Conteudo;
