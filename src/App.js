fc739170b74969cdce30ed75f844cbd383559725
José Francisco <franciscojosefrancisco341@gmail.com>
2023-10-20 09:27:37 -0300

    Organização para implementação de Rotas

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import "./App.css";
-import Cabecalho from "./componentes/Cabecalho";
-import CadastroUsuario from "./componentes/CadastroUsuario";
-import Conteudo from "./componentes/Conteudo";
-import LoginUsuario from "./componentes/LoginUsuario";
-import Rodape from "./componentes/Rodape";
-import Estados from "./exemplos/Estados";
+import Principal from "./paginas/Principal";
+
 /*
 Arquivo App JSX
 Componente funcional = porque ele é uma função
 */
 function App() {
   /*Todo componente deve retornar um html entre parenteses*/
-  return (
-    /*O React substitui className por class porque class é palavra resrvada do JS */
-    <>
-        
-      <Cabecalho logo="logo192.png" titulo="Fav.io" subtitulo="O melhor gerenciador de favoritos da Internet Brasileira!!!" />
-       <CadastroUsuario/>
-       <LoginUsuario/>      
-      {/* <Conteudo /> */}
-      {/*<Estados/>*/}
-      <Rodape/>
-    </>
-  );
+  return <Principal/> 
 }
 
 export default App;
